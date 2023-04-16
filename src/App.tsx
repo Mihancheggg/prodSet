1bcec795ec74007d3e1b39d30aa136f4fd1204d8
Mihail Schutow <schutow.mihail@yandex.ru>
2023-04-16 10:00:42 +0300

    Feat routing support

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Counter } from './components/counter/Counter';
 import './index.scss'
+import { Link, Route, Routes } from 'react-router-dom';
+import { MainPage } from './pages/MainPage/MainPage';
+import { AboutPage } from './pages/AboutPage/AboutPage';
 
 export const App = () => {
     return (
-        <div className='app'>
-            dfgdfg
-            <Counter/>
+        <div className="app">
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Routes>
+                <Route path={'/'} element={<MainPage/>}/>
+                <Route path={'/about'} element={<AboutPage/>}/>
+            </Routes>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 render(<div>
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 </div>, document.getElementById('root'))
