f3ff21c64974cc56954f94a4a97760aec5d11cfb
Mihail Schutow <schutow.mihail@yandex.ru>
2023-04-20 19:54:22 +0300

    Refactor App, set routes to their own config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import './styles/index.scss'
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/libs/classNames/classNames';
-import { MainPage } from 'pages/MainPage';
-import { AboutPage } from 'pages/AboutPage';
-
+import { AppRouter } from 'app/providers/router';
 
 
 export const App = () => {
@@ -16,12 +14,7 @@ export const App = () => {
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <button onClick={toggleTheme}>Toggle Theme</button>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage/>}/>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
