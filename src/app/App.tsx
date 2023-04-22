592f50b2132d6de2a5929fe64c2df6d973066dd1
Mihail Schutow <schutow.mihail@yandex.ru>
2023-04-22 11:04:48 +0300

    Feat Link widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './styles/index.scss'
-import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/libs/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 
 export const App = () => {
@@ -11,8 +11,7 @@ export const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+            <Navbar/>
             <button onClick={toggleTheme}>Toggle Theme</button>
             <AppRouter/>
         </div>
