dfd32e4fa276560582ad110952ebb2df7bca78d8
Mihail Schutow <schutow.mihail@yandex.ru>
2023-04-23 19:44:45 +0300

    Feat hot reloading

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export function buildPlugins({paths, isDev, mode, port}: BuildOptions): webpack.
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
